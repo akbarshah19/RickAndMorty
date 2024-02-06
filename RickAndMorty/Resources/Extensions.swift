4afd93bd8fc3eaa3e1694e5f518c417fb8fa1b38
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-02-06 22:46:35 +0500

    minor update

--- RickAndMorty/Resources/Extensions.swift
+++ RickAndMorty/Resources/Extensions.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 extension UIView {
-    func addSubviews(_ views: UIView...) {
+    public func addSubviews(_ views: UIView...) {
         views.forEach {
             addSubview($0)
         }
+    }
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
     }
 }
 
